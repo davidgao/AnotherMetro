f0ed018ab3d0a205a8f904655c22b13b368c581a
davidgao <davidgao1001@gmail.com>
2015-10-18 22:14:04 +0800

    fixup the tests

--- app/src/test/java/edu/fudan/davidgao/anothermetro/GameTest.java
+++ app/src/test/java/edu/fudan/davidgao/anothermetro/GameTest.java
@@ -1,9 +1,14 @@
 package edu.fudan.davidgao.anothermetro;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class GameTest {
+    private Game game;
+
+    /* Self test */
     @Test(expected=AssertionError.class)
     public void badAssertion() throws Exception {
         assertTrue(false);
@@ -14,16 +19,61 @@ public class GameTest {
         assertTrue(true);
     }
 
+    /* Tools */
+    @Before
+    public void buildup() {
+        try{
+            game = Game.create();
+        } catch (GameException exception) {
+            // Nothing
+        }
+    }
+
+    @After
+    public void teardown() {
+        Game game = Game.getInstance();
+        if (game != null) {
+            try {
+                game.kill();
+            } catch (GameException exception) {
+                // Nothing
+            }
+            try {
+                game.destroy();
+            } catch (GameException exception) {
+                // Nothing
+            }
+        }
+    }
+
     @Test
     public void getInstance() throws Exception {
-        Game game = Game.getInstance();
-        assertNull(game);
-        Game.create();
-        game = Game.getInstance();
         assertNotNull(game);
         game.kill();
         game.destroy();
         game = Game.getInstance();
         assertNull(game);
+    }
+
+    @Test
+    public void create() throws Exception {
+        assertNotNull(game);
+    }
+
+    @Test(expected=GameException.class)
+    public void badCreate() throws Exception {
+        Game.create();
+        Game.create();
+    }
+
+    @Test
+    public void start() throws Exception {
+        game.start();
+    }
+
+    @Test(expected=GameException.class)
+    public void badStart() throws Exception {
+        game.start();
+        game.start();
     }
 }
