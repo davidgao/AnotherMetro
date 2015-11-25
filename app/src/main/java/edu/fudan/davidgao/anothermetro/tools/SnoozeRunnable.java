85659d58ec982c17ff5e62a6425fe6e13baae16b
davidgao <davidgao1001@gmail.com>
2015-11-25 19:44:06 +0800

    Rewrite Matrix2D and apply as game map

---
+++ app/src/main/java/edu/fudan/davidgao/anothermetro/tools/SnoozeRunnable.java
@@ -0,0 +1,31 @@
+package edu.fudan.davidgao.anothermetro.tools;
+
+public class SnoozeRunnable {
+    /*
+        When alarm.run() is called, enter snoozing state.
+        When deadline.run() is called, run wrapped runnable if and only if in snoozing state.
+     */
+    public SnoozeRunnable(Runnable runnable) {
+        this.runnable = runnable;
+    }
+
+    public final Runnable alarm = new Runnable() {
+        @Override
+        public void run() {
+            snoozing = true;
+        }
+    };
+
+    public final Runnable deadline = new Runnable() {
+        @Override
+        public void run() {
+            if (snoozing) {
+                snoozing = false;
+                runnable.run();
+            }
+        }
+    };
+
+    private boolean snoozing = false;
+    private Runnable runnable;
+}
