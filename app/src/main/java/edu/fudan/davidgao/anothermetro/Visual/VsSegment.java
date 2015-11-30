544a5eaef7478bc6a441ec78f161ee8289188337
Gan Quan <coin2028@hotmail.com>
2015-12-01 01:56:10 +0800

    [gq] added interface request

--- app/src/main/java/edu/fudan/davidgao/anothermetro/Visual/VsSegment.java
+++ app/src/main/java/edu/fudan/davidgao/anothermetro/Visual/VsSegment.java
@@ -17,4 +17,14 @@ public class VsSegment {
         st_angle=0;ed_angle=0;
         st_a=0;ed_a=0;
     }
+    /*
+     * [gq] (try to) add the following interfaces:
+     * VsTrainState getTrainState(float fraction, int direction)
+     * @fraction: (x - st) / (ed - st)
+     * @direction: whether the train is moving in reverse.  1 if false, -1 if true.
+     * Returns a VsTrainState instance, which contains
+     * 1. final Point<Float> coordinate
+     * 2. final int angle
+     * skq and bob can directly use the instance to draw a train and passengers.
+     */
 }
